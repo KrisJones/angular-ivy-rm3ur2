f7bcd4269d24538444b29280e514b76e5ca8e3a6
KrisJones <kris.jones@live.co.uk>
2022-09-14 12:32:04 +0100

    1 file modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,9 @@
 
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
+import { Games } from './Games/games.component';
 
-const routes: Routes = [
-  { path: 'home', component: HomeComponent },
-  { path: 'dashboard', component: DashboardComponent },
-];
+const routes: Routes = [{ path: 'games', component: Games }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
